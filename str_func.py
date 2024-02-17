a4e22d83d38e892aefe1b58e21c8300e62dfb638
ferrum76 <daniil.zheleznyakov.2000@gmail.com>
2024-02-17 13:45:24 +0300

    add method title_symbol_to_upper and add docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
-def string_to_upper (s:str):
+def string_to_upper(s: str):
+    '''Метод конвектирует строку в строку из заглавных букв'''
     return s.upper()
+
+
+def title_symbol_to_upper(s):
+    '''Метод делает заглавными первые буквы каждого слова в строке, поступившей на вход функции'''
+    arr = s.split(" ")
+    for i in range(len(arr)):
+        arr[i] = arr[i][0].upper() + arr[i][1:]
+
+    return " ".join(arr)
